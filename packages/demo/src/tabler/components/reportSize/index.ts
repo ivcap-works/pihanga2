03a1ee44bd4cc40ba7d05fafab221119a20c0ab5
Max Ott <max.ott@data61.csiro.au>
2024-03-06 09:57:00 +1100

    demo: removed

--- packages/demo/src/tabler/components/reportSize/index.ts
+++
@@ -1,6 +0,0 @@
-import { PiRegister } from '@pihanga/core';
-export { Component as ReportSize } from './reportSize.component';
-export type { ComponentProps, SizeEventT } from './reportSize.component';
-
-export function init(_register: PiRegister): void {
-}
--- packages/demo/src/tabler/components/tbSubNavBar/index.ts
+++
@@ -1,6 +0,0 @@
-import { PiRegister } from '@pihanga/core';
-export { Component } from './subNavBar.component';
-export type { ComponentProps } from './subNavBar.component';
-
-export function init(_register: PiRegister): void {
-}
